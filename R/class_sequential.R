5029c4e517fcb3e489007b7a5bb60a72ab191cdc
wlandau-lilly <will.landau@lilly.com>
2020-07-08 21:50:18 -0400

    Sketch sequential class

---
+++ R/class_sequential.R
@@ -0,0 +1,33 @@
+sequential_init <- function(names = character(0)) {
+  sequential_new(names)
+}
+
+sequential_new <- function(data = NULL) {
+  sequential_class$new(data)
+}
+
+sequential_class <- R6::R6Class(
+  classname = "tar_sequential",
+  inherit = queue_class,
+  class = FALSE,
+  portable = FALSE,
+  cloneable = FALSE,
+  public = list(
+    dequeue = function() {
+      head <- self$data[1L]
+      self$data <- self$data[-1L]
+      head
+    },
+    prepend = function(names, ranks = NULL) {
+      self$data <- c(names, self$data)
+    },
+    increment_ranks = function(names = NULL, by = NULL) {
+    },
+    should_dequeue = function() {
+      self$is_nonempty()
+    },
+    validate = function() {
+      assert_chr(self$data)
+    }
+  )
+)
