5da88ba8b8b9d46933afddaad1430b4de672f6d1
wlandau-lilly <will.landau@lilly.com>
2020-07-14 13:55:43 -0400

    Add performance tests

--- tests/performance/test-stems.R
+++ tests/performance/test-stems.R
@@ -14,13 +14,29 @@ tar_destroy()
 
 # With API
 tar_script({
-  targets <- lapply(
-    as.character(seq_len(1e3)), function(name) {
-      tar_target_external(name, expr = quote(1 + 1))
-    }
-  )
+  targets <- lapply(seq_len(1e3), function(id) {
+    name <- paste0("x", as.character(id))
+    dep <- paste0("x", as.character(id - 1L))
+    expr <- as.expression(rlang::sym(dep))
+    tar_target_external(name, expr = expr)
+  })
   tar_pipeline(targets)
 })
 px <- pprof(tar_make(reporter = "summary", callr_function = NULL))
+tar_destroy()
+unlink("_targets.R")
+
+# Same, but just setup overhead
+tar_script({
+  target_x0 <- tar_target(x0, stop())
+  targets <- lapply(seq_len(1e3), function(id) {
+    name <- paste0("x", as.character(id))
+    dep <- paste0("x", as.character(id - 1L))
+    expr <- as.expression(rlang::sym(dep))
+    tar_target_external(name, expr = expr)
+  })
+  tar_pipeline(c(targets, target_x0))
+})
+px <- pprof(try(tar_make(reporter = "summary", callr_function = NULL)))
 tar_destroy()
 unlink("_targets.R")
