99ee43081b492840c7d48a05a54bfadbba48b73e
wlandau <will.landau@gmail.com>
2020-08-29 00:08:40 -0400

    Add profiling study for speed of large data

---
+++ tests/performance/test-data.R
@@ -0,0 +1,19 @@
+library(proffer)
+devtools::load_all()
+tar_destroy()
+tar_option_set(memory = "transient")
+targets <- lapply(
+  seq_len(50),
+  function(index) {
+    tar_target_raw(
+      as.character(index),
+      quote(data.frame(x = runif(1e7))),
+      memory = "transient",
+      format = "fst"
+    )
+  }
+)
+pipeline <- tar_pipeline(targets)
+local <- local_init(pipeline)
+px <- pprof(local$run())
+tar_destroy()
