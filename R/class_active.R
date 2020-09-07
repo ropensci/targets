616251e66d789ef2e775fa19d62dd87b74ab571b
wlandau <will.landau@gmail.com>
2020-09-07 08:40:10 -0400

    Hide R6 classes

--- R/class_active.R
+++ R/class_active.R
@@ -1,15 +1,3 @@
-#' @title Active algorithm constructor.
-#' @export
-#' @keywords internal
-#' @description Not a user-side function.
-#'   Used to build external HPC scheduling algorithms.
-#' @param pipeline Pipeline object.
-#' @param meta Meta object.
-#' @param names Character, names of targets.
-#' @param queue Character, type of queue.
-#' @param reporter Character, type of reporter.
-#' @param garbage_collection Logical, whether to
-#'   periodically run garbage collection.
 active_new <- function(
   pipeline = NULL,
   meta = NULL,
@@ -28,12 +16,6 @@ active_new <- function(
   )
 }
 
-#' @title Abstract class for active algorithm objects.
-#' @aliases tar_active
-#' @export
-#' @keywords internal
-#' @description Not a user-side R6 class.
-#'   Used to build external HPC scheduling algorithms.
 active_class <- R6::R6Class(
   classname = "tar_active",
   inherit = algorithm_class,
@@ -41,17 +23,7 @@ active_class <- R6::R6Class(
   portable = FALSE,
   cloneable = FALSE,
   public = list(
-    #' @field garbage_collection Logical, whether to
-    #'   periodically run garbage collection.
     garbage_collection = NULL,
-    #' @description Initialize an active algorithm object.
-    #' @param pipeline Pipeline object.
-    #' @param meta Meta object.
-    #' @param names Character, names of targets.
-    #' @param queue Character, type of queue.
-    #' @param reporter Character, type of reporter.
-    #' @param garbage_collection Logical, whether to
-    #'   periodically run garbage collection.
     initialize = function(
       pipeline = NULL,
       meta = NULL,
@@ -69,29 +41,22 @@ active_class <- R6::R6Class(
       )
       self$garbage_collection <- garbage_collection
     },
-    #' @description Ensure metadata is pre-processed
-    #'   and up to date in memory.
     ensure_meta = function() {
       self$meta$database$preprocess(write = TRUE)
       envir <- pipeline_get_envir(self$pipeline)
       self$meta$record_imports(envir, self$pipeline)
     },
-    #' @description Run garbage collection if enabled.
     run_gc = function() {
       if (self$garbage_collection) {
         gc()
       }
     },
-    #' @description Unload transient-memory targets.
     unload_transient = function() {
       pipeline_unload_transient(self$pipeline)
     },
-    #' @description Unserialize a target.
-    #' @param target Target object.
     unserialize_target = function(target) {
       builder_unserialize_value(target)
     },
-    #' @description Start the algorithm.
     start = function() {
       pipeline_prune_names(self$pipeline, self$names)
       self$update_scheduler()
@@ -99,7 +64,6 @@ active_class <- R6::R6Class(
       self$scheduler$progress$database$reset_storage()
       self$scheduler$reporter$report_start()
     },
-    #' @description End the algorithm.
     end = function() {
       pipeline_unload_loaded(self$pipeline)
       scheduler <- self$scheduler
@@ -107,7 +71,6 @@ active_class <- R6::R6Class(
       store_del_scratch()
       self$run_gc()
     },
-    #' @description Validate the algorithm.
     validate = function() {
       super$validate()
       assert_lgl(self$garbage_collection)
