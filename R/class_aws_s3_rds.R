ec3255a5a0db5e76ea37043a3670703e8157d18a
wlandau <will.landau@gmail.com>
2020-09-27 12:02:56 -0400

    Fix signature

--- R/class_aws_s3_rds.R
+++ R/class_aws_s3_rds.R
@@ -1,4 +1,4 @@
-store_new.aws_rds <- function(file = NULL, resources = NULL) {
+store_new.aws_rds <- function(class, file = NULL, resources = NULL) {
   force(file)
   force(resources)
   enclass(environment(), c("tar_aws_s3", "tar_rds", "tar_store"))
