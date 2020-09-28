bdfef0e1c27265525bfef5857fe99d8bdd10418d
wlandau <will.landau@gmail.com>
2020-09-28 14:11:07 -0400

    Reinstate tar_dir()

--- tests/testthat/helper-utils.R
+++ tests/testthat/helper-utils.R
@@ -1,12 +1,9 @@
 tar_test <- function(label, code) {
   code <- substitute(code)
   expr <- substitute(
-    testthat::test_that(label, code),
+    tar_dir(testthat::test_that(label, code)),
     env = list(label = label, code = code)
   )
-  dir <- tempfile(pattern = "targets_")
-  dir_create(dir)
-  withr::local_dir(dir)
   withr::local_envvar(c(TAR_ASK = "false"))
   tar_option_reset()
   tar_option_set(envir = new.env(parent = globalenv()))
