19f3bdc1c18eadc0944c3efbbdc8b17a526bdadd
wlandau <will.landau@gmail.com>
2020-09-27 22:03:38 -0400

    Skip long aws tests on cran

--- tests/testthat/helper-aws.R
+++ tests/testthat/helper-aws.R
@@ -4,6 +4,7 @@ skip_if_no_aws <- function() {
   skip_if(Sys.getenv("AWS_DEFAULT_REGION") == "")
   skip_if_not_installed("aws.s3")
   skip_if_offline()
+  skip_on_cran()
 }
 
 random_bucket_name <- function() {
