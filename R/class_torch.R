be68c5be9c6fc346fecb57a3f8421e46b2a4b154
wlandau <will.landau@gmail.com>
2020-10-01 10:44:56 -0400

    Re-automate torch tests

--- R/class_torch.R
+++ R/class_torch.R
@@ -13,10 +13,6 @@ torch_new <- function(file = NULL, resources = NULL) {
 store_assert_format_setting.torch <- function(class) {
 }
 
-# Tested in tests/interactive/test-torch.R.
-# Wanted to automate these tests, but there is a mysterious segfault
-# on GitHub Actions: https://github.com/wlandau/targets/runs/1193098948 # nolint
-# nocov start
 #' @export
 store_read_path.tar_torch <- function(store, path) {
   torch::torch_load(path)
@@ -41,7 +37,6 @@ store_unserialize_value.tar_torch <- function(store, value) {
   on.exit(close(con))
   value$object <- torch::torch_load(con)
 }
-# nocov end
 
 #' @export
 store_validate_packages.tar_torch <- function(store) {
