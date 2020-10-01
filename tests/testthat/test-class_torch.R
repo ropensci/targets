f974fd47ee873db8693b3887029eaf01b13e3bc2
wlandau <will.landau@gmail.com>
2020-10-01 07:37:34 -0400

    Simplify test

--- tests/testthat/test-class_torch.R
+++ tests/testthat/test-class_torch.R
@@ -1,33 +1,30 @@
 tar_test("torch format", {
+  skip_on_cran()
   skip_if_not_installed("torch")
   tar_script({
-    f <- function() {
-      x <- array(runif(8), dim = c(2, 2, 2))
-      torch::torch_tensor(x, dtype = torch::torch_float64())
-    }
-    tar_pipeline(tar_target(abc, f(), format = "torch"))
+    tar_pipeline(tar_target(a, torch::torch_tensor(c(1, 2)), format = "torch"))
   })
   tar_make(callr_function = NULL)
-  out <- tar_read(abc)
+  out <- tar_read(a)
   expect_true(inherits(out, "torch_tensor"))
+  expect_equal(torch::as_array(out), c(1, 2))
 })
 
 tar_test("torch format with in-memory serialization", {
+  skip_on_cran()
   skip_if_not_installed("torch")
+  future::plan(future::sequential)
   tar_script({
-    f <- function() {
-      x <- array(runif(8), dim = c(2, 2, 2))
-      torch::torch_tensor(x, dtype = torch::torch_float64())
-    }
-    tar_pipeline(tar_target(abc, f(), format = "torch"))
+    tar_pipeline(tar_target(a, torch::torch_tensor(c(1, 2)), format = "torch"))
   })
   tar_make_future(callr_function = NULL)
-  out <- tar_read(abc)
+  out <- tar_read(a)
   expect_true(inherits(out, "torch_tensor"))
+  expect_equal(torch::as_array(out), c(1, 2))
 })
 
 tar_test("validate torch format", {
   skip_if_not_installed("torch")
-  x <- target_init(name = "abc", expr = quote(f()), format = "torch")
+  x <- target_init(name = "a", expr = quote(f()), format = "torch")
   expect_silent(target_validate(x))
 })
