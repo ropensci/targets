c6c44a42453d7fe97afad6dab949f02e5ae3d473
wlandau <will.landau@gmail.com>
2020-10-01 15:34:13 -0400

    Fix interactive test

--- tests/interactive/test-keras.R
+++ tests/interactive/test-keras.R
@@ -42,7 +42,7 @@ tar_test("keras format", {
   expect_true(
     inherits(x$value$object, "keras.engine.training.Model")
   )
-  builder_update_path(x)
+  builder_update_paths(x)
   builder_update_object(x)
   expect_true(
     inherits(target_read_value(x)$object, "keras.engine.training.Model")
