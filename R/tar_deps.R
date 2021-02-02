383c425891ae37ef103f49cc9b2ff9a8c9373bf4
wlandau <will.landau@gmail.com>
2021-02-02 06:45:00 -0500

    Reorganize functions

--- R/tar_deps.R
+++ R/tar_deps.R
@@ -17,25 +17,3 @@ tar_deps <- function(expr) {
   expr <- substitute(expr)
   tar_deps_raw(expr)
 }
-
-#' @title Code dependencies (raw version)
-#' @export
-#' @description Same as [tar_deps()] except `expr` must already be an
-#'   unquoted function or expression object.
-#' @return Character vector of the dependencies of a function or expression.
-#' @param expr An R expression object or function.
-#' @examples
-#' tar_deps_raw(quote(x <- y + z))
-#' tar_deps_raw(
-#'   quote({
-#'     x <- 1
-#'     x + a
-#'   })
-#' )
-#' tar_deps_raw(function(a = b) map_dfr(data, ~do_row(.x)))
-tar_deps_raw <- function(expr) {
-  if (!is.function(expr)) {
-    expr <- embody_expr(expr)
-  }
-  deps_function(expr)
-}
