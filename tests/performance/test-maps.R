9491e37bc4ef439219a588591d96e4f36a2beb06
wlandau <will.landau@gmail.com>
2021-02-26 21:42:42 -0500

    Comment tests

--- tests/performance/test-maps.R
+++ tests/performance/test-maps.R
@@ -1,3 +1,4 @@
+# Start in a new R session.
 library(proffer)
 
 # Should barely see target_load_deps() in the profiling data.
@@ -13,7 +14,7 @@ px <- pprof(tar_make(reporter = "summary", callr_function = NULL))
 # Outdated targets
 px <- pprof(tar_outdated(callr_function = NULL))
 
-# Should see more time on target_load_deps() in the profiling data.
+# Should see more time on target_ensure_deps() in the profiling data.
 # Use debug(store_read_path.tar_rds) to be sure readRDS() is called. # nolint
 tar_destroy()
 tar_script({
