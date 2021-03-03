acebfaf10f7cf21acceea2f28899fcb7b9a301d1
wlandau <will.landau@gmail.com>
2021-03-03 11:05:10 -0500

    Add a backoff test

--- tests/testthat/test-class_backoff.R
+++ tests/testthat/test-class_backoff.R
@@ -1,8 +1,8 @@
-test_that("backoff$validate()", {
+tar_test("backoff$validate()", {
   expect_silent(backoff_init()$validate())
 })
 
-test_that("index and intervals", {
+tar_test("index and intervals", {
   x <- backoff_init(min = 1, rate = 2, max = 50)
   expect_equal(x$index, 0L)
   expect_equal(x$interval_base(), x$min)
@@ -22,11 +22,25 @@ test_that("index and intervals", {
   expect_lt(x$interval(), 1.11)
 })
 
-test_that("maximum base interval", {
+tar_test("maximum base interval", {
   set.seed(0)
   x <- backoff_init(min = 1, rate = 2, max = 10)
   map(seq_len(100), ~x$increment())
   expect_equal(x$interval_base(), 10)
+  expect_gt(x$interval(), x$interval_base())
+  expect_lt(x$interval() - 1, x$interval_base())
+})
+
+tar_test("sleep increments index", {
+  set.seed(0)
+  x <- backoff_init(min = 0.001, rate = 2, max = 10)
+  expect_equal(x$index, 0L)
+  expect_equal(x$interval_base(), 0.001)
+  expect_gt(x$interval(), x$interval_base())
+  expect_lt(x$interval() - 1, x$interval_base())
+  x$sleep()
+  expect_equal(x$index, 1L)
+  expect_equal(x$interval_base(), 0.002)
   expect_gt(x$interval(), x$interval_base())
   expect_lt(x$interval() - 1, x$interval_base())
 })
