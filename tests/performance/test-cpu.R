e89feade4712c0a4eea8654b32e1fc694f6a30ce
wlandau <will.landau@gmail.com>
2021-03-05 10:34:23 -0500

    Add clustermq cpu test

--- tests/performance/test-cpu.R
+++ tests/performance/test-cpu.R
@@ -11,3 +11,15 @@ tar_script({
 # CPU usage should drop down to almost nothing
 # as exponential backoff kicks in.
 tar_make_future(callr_function = NULL)
+
+# Restart R and monitor CPU usage.
+rstudioapi::restartSession()
+library(targets)
+tar_script({
+  library(targets)
+  options(clustermq.scheduler = "multiprocess")
+  list(tar_target(x, Sys.sleep(360)))
+})
+
+# CPU usage should drop down to almost nothing.
+tar_make_clustermq(callr_function = NULL)
