008716f5b9920bc553fbdfef3d70ab7632a4c27b
wlandau <will.landau@gmail.com>
2021-03-23 20:02:33 -0400

    Fix #355

---
+++ R/class_feather.R
@@ -0,0 +1,38 @@
+#' @export
+store_new.feather <- function(class, file = NULL, resources = NULL) {
+  feather_new(file, resources)
+}
+
+feather_new <- function(file = NULL, resources = NULL) {
+  force(file)
+  force(resources)
+  enclass(environment(), c("tar_feather", "tar_store"))
+}
+
+#' @export
+store_assert_format_setting.feather <- function(class) {
+}
+
+#' @export
+store_read_path.tar_feather <- function(store, path) {
+  arrow::read_feather(path)
+}
+
+#' @export
+store_write_path.tar_feather <- function(store, object, path) {
+  args <- list(x = object, sink = path, version = 2L)
+  args$compression <- store$resources$compression
+  args$compression_level <- store$resources$compression_level
+  do.call(arrow::write_feather, args)
+}
+
+#' @export
+store_coerce_object.tar_feather <- function(store, object) {
+  assert_df(object, "target with format = \"feather\" must be a data frame.")
+  object
+}
+
+#' @export
+store_get_packages.tar_feather <- function(store) {
+  "arrow"
+}
