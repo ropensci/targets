0c8de5c004698e3e19dbb2b14d19f9d82ee8ee12
wlandau <will.landau@gmail.com>
2021-03-30 15:38:06 -0400

    Bring back spinner for non-summary reporters

--- R/utils_callr.R
+++ R/utils_callr.R
@@ -94,6 +94,6 @@ callr_args_default <- function(callr_function, reporter = NULL) {
   if (is.null(callr_function)) {
     return(list())
   }
-  out <- list(spinner = identical(reporter, "silent"))
+  out <- list(spinner = !identical(reporter, "summary"))
   out[intersect(names(out), names(formals(callr_function)))]
 }
