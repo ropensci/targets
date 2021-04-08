1472267db784018767f38212c0c1acbb1f1856c7
wlandau <will.landau@gmail.com>
2021-04-08 09:03:27 -0400

    Fix lint

--- tests/testthat/test-class_aws_rds.R
+++ tests/testthat/test-class_aws_rds.R
@@ -22,7 +22,7 @@ tar_test("store_path_from_record()", {
 
 tar_test("store_produce_path()", {
   store <- tar_target(x, "x_value", format = "aws_rds")$store
-  store$resources = list(bucket = "x_bucket")
+  store$resources <- list(bucket = "x_bucket")
   out <- store_produce_path(store, "x_name", "x_object")
   expect_equal(out, c("x_bucket", "_targets/objects/x_name"))
 })
