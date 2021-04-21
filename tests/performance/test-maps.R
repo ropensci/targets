e08ad7d2839f547ae4cda27b1cb7ec330c8bd24b
wlandau <will.landau@gmail.com>
2021-04-20 21:47:19 -0400

    Add a profiling study

--- tests/performance/test-maps.R
+++ tests/performance/test-maps.R
@@ -4,6 +4,7 @@ library(proffer)
 # Should barely see target_load_deps() in the profiling data.
 # Use debug(store_read_path.tar_rds) to be sure readRDS() is not called. # nolint
 tar_script({
+  options(clustermq.scheduler = "multicore")
   list(
     tar_target(x, seq_len(1e3)),
     tar_target(y, x, pattern = map(x))
@@ -14,6 +15,16 @@ px <- pprof(tar_make(reporter = "summary", callr_function = NULL))
 # With silent reporter
 tar_destroy()
 px <- pprof(tar_make(reporter = "silent", callr_function = NULL))
+
+# With clustermq
+tar_destroy()
+px <- pprof(
+  tar_make_clustermq(workers = 8, reporter = "silent", callr_function = NULL)
+)
+
+# With future
+tar_destroy()
+px <- pprof(tar_make_future(reporter = "silent", callr_function = NULL))
 
 # Outdated targets
 px <- pprof(tar_outdated(callr_function = NULL))
