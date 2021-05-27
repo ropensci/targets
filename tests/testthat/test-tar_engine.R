8e4cfc97a2a2282a9c04eb3e36b93778d00092ea
wlandau <will.landau@gmail.com>
2021-05-27 14:58:52 -0400

    Add another test

--- tests/testthat/test-tar_engine.R
+++ tests/testthat/test-tar_engine.R
@@ -10,6 +10,7 @@ tar_test("tar_engine() construct globals", {
   )
   tar_engine(options, prototype = FALSE)
   expect_true(file.exists(path_script()))
+  expect_false(file.exists(path_script_r_targets_dir()))
   expect_true(file.exists(path_script_r_globals("test")))
   tar_make(callr_function = NULL)
   expect_equal(x, "a")
@@ -32,7 +33,26 @@ tar_test("tar_engine() prototype globals", {
   expect_false(exists("x", envir = envir, inherits = FALSE))
   tar_engine(options, prototype = TRUE)
   expect_false(file.exists(path_script()))
+  expect_false(file.exists(path_script_r_targets_dir()))
   expect_false(file.exists(path_script_r_globals("test")))
   expect_true(exists("x", envir = envir, inherits = FALSE))
   expect_equal(envir$x, "a")
+})
+
+tar_test("tar_engine() construct targets", {
+  skip_if_not_installed("knitr")
+  options <- list(
+    code = "tar_target(x, \"a\")",
+    echo = FALSE,
+    engine = "targets",
+    label = "test",
+    results = "hide",
+    targets = TRUE
+  )
+  tar_engine(options, prototype = FALSE)
+  expect_true(file.exists(path_script()))
+  expect_false(file.exists(path_script_r_globals_dir()))
+  expect_true(file.exists(path_script_r_targets("test")))
+  tar_make(callr_function = NULL)
+  expect_equal(tar_read(x), "a")
 })
