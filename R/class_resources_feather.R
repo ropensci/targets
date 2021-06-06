2e8917adba1ba3c201254c2c922450beb37a3d6f
wlandau <will.landau@gmail.com>
2021-06-05 22:57:28 -0400

    Sketch feather resources class

---
+++ R/class_resources_feather.R
@@ -0,0 +1,32 @@
+resources_init <- function(
+  compression = character(0),
+  compression_level = NULL
+) {
+  resources_new(
+    compression = compression,
+    compression_level = compression_level
+  )
+}
+
+resources_new <- function(
+  compression = NULL,
+  compression_level = NULL
+) {
+  force(compression)
+  force(compression_level)
+  enclass(environment(), "tar_resources_feather")
+}
+
+#' @export
+resources_validate.tar_resources_feather <- function(resources) {
+  assert_scalar(resources$compression)
+  assert_chr(resources$compression)
+  assert_nzchar(resources$compression)
+  assert_scalar(resources$compression_level %|||% 1)
+  assert_dbl(resources$compression_level %|||% 1)
+}
+
+#' @export
+print.tar_resources_feather <- function(x, ...) {
+  cat("<feather>\n ", paste0(paste_list(as.list(x)), collapse = "\n  "))
+}
