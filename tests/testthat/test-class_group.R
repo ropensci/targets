124e425cafd84bb28c92899d92709fb7d27377ce
wlandau <will.landau@lilly.com>
2021-06-10 21:46:55 -0400

    Make pattern more specific

--- tests/testthat/test-class_group.R
+++ tests/testthat/test-class_group.R
@@ -100,7 +100,7 @@ tar_test("group branch invalidation (#507)", {
   })
   tar_make(callr_function = NULL)
   out <- tar_progress()
-  out <- out$name[out$progress == "built" & grepl("_", out$name)]
+  out <- out$name[out$progress == "built" & grepl("^y_", out$name)]
   expect_equal(length(out), 1L)
 })
 
