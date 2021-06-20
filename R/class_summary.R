786cbd8640979d7480e207317d4fcc69b8b48def
wlandau <will.landau@gmail.com>
2021-06-19 21:46:16 -0400

    Speed up summary reporter (only print every 0.25s)

--- R/class_summary.R
+++ R/class_summary.R
@@ -9,25 +9,34 @@ summary_class <- R6::R6Class(
   portable = FALSE,
   cloneable = FALSE,
   public = list(
+    time = NULL,
+    report_start = function() {
+      self$time <- proc.time()["elapsed"]
+      cli_df_header(progress_init(path = tempfile())$cli_data())
+    },
+    report_progress = function(progress) {
+      time <- proc.time()["elapsed"]
+      if (time - self$time > 0.25) {
+        cli_df_body(progress$cli_data())
+        self$time <- time
+      }
+    },
     report_error = function(error) {
     },
-    report_start = function() {
-      cli_df_header(progress_init()$cli_data())
-    },
     report_started = function(target = NULL, progress) {
-      cli_df_body(progress$cli_data())
+      self$report_progress(progress)
     },
     report_built = function(target = NULL, progress) {
-      cli_df_body(progress$cli_data())
+      self$report_progress(progress)
     },
     report_skipped = function(target = NULL, progress) {
-      cli_df_body(progress$cli_data())
+      self$report_progress(progress)
     },
     report_errored = function(target = NULL, progress) {
-      cli_df_body(progress$cli_data())
+      self$report_progress(progress)
     },
     report_canceled = function(target = NULL, progress) {
-      cli_df_body(progress$cli_data())
+      self$report_progress(progress)
     },
     report_end = function(progress) {
       cli_df_body(progress$cli_data())
