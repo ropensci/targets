46966b87883c07260c30146f2e231844f47e5c99
wlandau <will.landau@gmail.com>
2021-06-21 19:45:21 -0400

    Fix lint

--- R/utils_assert.R
+++ R/utils_assert.R
@@ -87,7 +87,7 @@ tar_assert_df <- function(x, msg = NULL) {
 
 #' @export
 #' @rdname tar_assert
-tar_assert_equal_lengths <- function (x, msg = NULL) {
+tar_assert_equal_lengths <- function(x, msg = NULL) {
   lengths <- map_int(x, length)
   if (length(unique(lengths)) > 1L) {
     targets::tar_throw_validate(msg %|||% "x must have equal-length elements.")
