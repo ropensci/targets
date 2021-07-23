cb20f6122f313f2f1009bc7749a5376274fdbcab
wlandau <will.landau@gmail.com>
2021-07-23 08:11:43 -0400

    Edit test

--- tests/hpc/test-future_error.R
+++ tests/hpc/test-future_error.R
@@ -1,8 +1,12 @@
 library(targets)
 tar_script({
   future::plan(future.callr::callr)
-  tar_target(x, Sys.sleep(400), error = "continue")
+  list(
+    tar_target(x, Sys.sleep(400), error = "continue"),
+    tar_target(y, x)
+  )
 })
 tar_make_future()
-# Terminate the worker. Should show a callr error message:
+# Terminate the worker.
+# Target y should start and target x should show a callr error message:
 tar_meta(x, error)$error
