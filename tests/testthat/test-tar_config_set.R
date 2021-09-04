f870af0cb603611f112f40a1b2c19999f3805284
wlandau <will.landau@gmail.com>
2021-09-04 17:35:31 -0400

    Fix #623

--- tests/testthat/test-tar_config_set.R
+++ tests/testthat/test-tar_config_set.R
@@ -220,17 +220,6 @@ tar_test("project switching/setting with project arg", {
   tar_config_set(store = "xyz", project = "project2")
   expect_equal(tar_config_get("store", project = "project1"), "abc")
   expect_equal(tar_config_get("store", project = "project2"), "xyz")
-  tar_config_unset("store", project = "project1")
-  expect_equal(
-    tar_config_get("store", project = "project1"),
-    path_store_default()
-  )
-  expect_equal(tar_config_get("store", project = "project2"), "xyz")
-  tar_config_unset("store", project = "project2")
-  expect_equal(
-    tar_config_get("store", project = "project2"),
-    path_store_default()
-  )
 })
 
 tar_test("project switching/setting with TAR_PROJECT", {
@@ -244,12 +233,6 @@ tar_test("project switching/setting with TAR_PROJECT", {
   Sys.setenv(TAR_PROJECT = "project2")
   expect_equal(tar_config_get("store"), "xyz")
   Sys.setenv(TAR_PROJECT = "project1")
-  tar_config_unset("store")
-  expect_equal(tar_config_get("store"), path_store_default())
-  Sys.setenv(TAR_PROJECT = "project2")
-  expect_equal(tar_config_get("store"), "xyz")
-  tar_config_unset("store")
-  expect_equal(tar_config_get("store"), path_store_default())
 })
 
 tar_test("correct project inheritance (1 level)", {
--- tests/testthat/test-tar_config_unset.R
+++ tests/testthat/test-tar_config_unset.R
@@ -12,10 +12,44 @@ tar_test("tar_config_unset()", {
   expect_equal(tar_config_get("workers"), workers + 500L)
 })
 
-tar_test("tar_config_unset() unsets from correct project", {
-  stop("Remember to implement!")
+tar_test("tar_config_unset() unsets from correct project (arg)", {
+  tar_config_set(store = "abc", project = "project1")
+  tar_config_set(store = "xyz", project = "project2")
+  tar_config_unset("store", project = "project1")
+  expect_equal(
+    tar_config_get("store", project = "project1"),
+    path_store_default()
+  )
+  expect_equal(tar_config_get("store", project = "project2"), "xyz")
+  tar_config_unset("store", project = "project2")
+  expect_equal(
+    tar_config_get("store", project = "project2"),
+    path_store_default()
+  )
+})
+
+tar_test("tar_config_unset() unsets from correct project (env var)", {
+  on.exit(Sys.unsetenv("TAR_PROJECT"))
+  Sys.setenv(TAR_PROJECT = "project1")
+  tar_config_set(store = "abc")
+  Sys.setenv(TAR_PROJECT = "project2")
+  tar_config_set(store = "xyz")
+  Sys.setenv(TAR_PROJECT = "project1")
+  tar_config_unset("store")
+  expect_equal(tar_config_get("store"), path_store_default())
+  Sys.setenv(TAR_PROJECT = "project2")
+  expect_equal(tar_config_get("store"), "xyz")
+  tar_config_unset("store")
+  expect_equal(tar_config_get("store"), path_store_default())
 })
 
 tar_test("tar_config_unset() converts to multi-project format", {
-  stop("Remember to implement!")
+  writeLines("store: abc\nscript: x123", "_targets.yaml")
+  expect_warning(
+    tar_config_unset("store"),
+    class = "tar_condition_deprecate"
+  )
+  expect_equal(tar_config_get("script"), "x123")
+  out <- yaml::read_yaml("_targets.yaml")
+  expect_equal(out, list(main = list(script = "x123")))
 })
