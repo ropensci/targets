fadb9cab7721f4faaeca286be716d240fc19e42b
wlandau <will.landau@gmail.com>
2021-11-03 11:45:56 -0400

    Fix coverage

--- tests/testthat/test-class_aws.R
+++ tests/testthat/test-class_aws.R
@@ -19,3 +19,13 @@ tar_test("metabucket compat with targets <= 0.8.1", {
   expect_equal(store_aws_bucket(path), "bucket_name")
   expect_null(store_aws_region(path))
 })
+
+tar_test("store_aws_key()", {
+  path <- c("bucket_name", "key_name", "stage_name")
+  expect_equal(store_aws_key(path), "key_name")
+})
+
+tar_test("store_aws_path()", {
+  path <- c("bucket_name", "key_name", "stage_name")
+  expect_equal(store_aws_path(path), "stage_name")
+})
