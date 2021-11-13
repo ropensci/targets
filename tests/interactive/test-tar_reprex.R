52539ebce0349aaf08db6e845fd2abbb9c14527f
wlandau <will.landau@gmail.com>
2021-11-13 09:34:08 -0500

    Update test

--- tests/interactive/test-tar_reprex.R
+++ tests/interactive/test-tar_reprex.R
@@ -1,2 +1,27 @@
-targets::tar_reprex() # Should create a reprex.
-targets::tar_reprex() # Graph should still show outdated targets.
+# Should create a reprex.
+tar_reprex(
+  pipeline = {
+    list(
+      tar_target(data, data.frame(x = sample.int(1e3))),
+      tar_target(summary, mean(data$x, na.rm = TRUE))
+    )
+  },
+  run = {
+    tar_visnetwork()
+    tar_make()
+  }
+)
+
+# Graph should still show outdated targets.
+tar_reprex(
+  pipeline = {
+    list(
+      tar_target(data, data.frame(x = sample.int(1e3))),
+      tar_target(summary, mean(data$x, na.rm = TRUE))
+    )
+  },
+  run = {
+    tar_visnetwork()
+    tar_make()
+  }
+)
