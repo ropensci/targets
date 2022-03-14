5ae7525abc36f9493b2e8cc436a7fcb05a172110
wlandau <will.landau@gmail.com>
2022-03-13 20:21:40 -0400

    Add an aws test

--- tests/aws/test-deprecated.R
+++ tests/aws/test-deprecated.R
@@ -60,3 +60,39 @@ tar_test("deprecated format = \"aws_parquet\"", {
   out <- tar_read(x)
   expect_equal(out, data.frame(x = seq_len(2), y = seq_len(2)))
 })
+
+
+tar_test("migrate meta database", {
+  skip_if_no_aws()
+  skip_if_not_installed("arrow")
+  s3 <- paws::s3()
+  bucket_name <- random_bucket_name()
+  s3$create_bucket(Bucket = bucket_name)
+  on.exit(destroy_bucket(bucket_name))
+  expr <- quote({
+    tar_option_set(
+      resources = tar_resources(
+        aws = tar_resources_aws(bucket = !!bucket_name)
+      ),
+      format = "parquet",
+      repository = "aws"
+    )
+    list(
+      tar_target(x, data.frame(x = seq_len(2), y = seq_len(2)))
+    )
+  })
+  expr <- tar_tidy_eval(expr, environment(), TRUE)
+  eval(as.call(list(`tar_script`, expr, ask = FALSE)))
+  tar_make(callr_function = NULL)
+  expect_equal(tar_outdated(callr_function = NULL), character(0))
+  meta <- meta_init()
+  data <- as_data_frame(meta$database$read_condensed_data())
+  expect_false(is.null(data$repository))
+  data$repository <- NULL
+  expect_true(is.null(data$repository))
+  data$format[!is.na(data$format)] <- "aws_parquet"
+  meta$database$overwrite_storage(data)
+  expect_equal(tar_outdated(callr_function = NULL), character(0))
+  out <- tar_read(x)
+  expect_equal(out, data.frame(x = seq_len(2), y = seq_len(2)))
+})
