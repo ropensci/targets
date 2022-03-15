fa7cb34a5226c5fb3eead9a0b5734ae2c8916727
wlandau <will.landau@gmail.com>
2022-03-15 13:39:17 -0400

    Add tar_Resources_gcp()

--- R/tar_resources_gcp.R
+++ R/tar_resources_gcp.R
@@ -1,40 +1,34 @@
-#' @title Target resources: Google Cloud Platform storage formats
+#' @title Target resources: Google Cloud Platform (GCP)
+#'   Google Cloud Storage (GCS)
 #' @export
 #' @family resources
 #' @description Create the `gcp` argument of `tar_resources()`
-#'   to specify optional settings to gcp storage formats.
+#'   to specify optional settings to gcp for
+#'   `tar_target(..., repository = "gcp")`.
 #'   See the `format` argument of [tar_target()] for details.
 #' @inheritSection tar_resources Resources
 #' @return Object of class `"tar_resources_gcp"`, to be supplied
 #'   to the `gcp` argument of `tar_resources()`.
-#' @param bucket Character of length 1, name of an existing
-#'   gcp bucket to upload and download the return values
-#'   of the affected targets during the pipeline.
-#' @param prefix Character of length 1, "directory path"
-#'   in the S3 bucket where the target return values are stored.
-#' @param verbose Whether to have feedback on the GCS upload process
+#' @inheritParams tar_resources_aws
 #' @examples
 #' # Somewhere in you target script file (usually _targets.R):
-#' if (identical(Sys.getenv("TAR_EXAMPLES"), "true")) {
 #' tar_target(
 #'   name,
 #'   command(),
-#'   format = "gcp_qs",
+#'   format = "qs",
+#'   repository = "gcp",
 #'   resources = tar_resources(
 #'     gcp = tar_resources_gcp(bucket = "yourbucketname"),
 #'     qs = tar_resources_qs(preset = "fast")
 #'   )
 #' )
-#' }
 tar_resources_gcp <- function(
   bucket,
-  prefix = targets::path_objects_dir_cloud(),
-  verbose = FALSE
+  prefix = targets::path_objects_dir_cloud()
 ) {
   out <- resources_gcp_init(
     bucket = bucket,
-    prefix = prefix,
-    verbose = verbose
+    prefix = prefix
   )
   resources_validate(out)
   out
