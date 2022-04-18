3c468cf816e35d6175b9bf1dcb78bcca62bd85fc
wlandau <will.landau@gmail.com>
2022-04-18 09:38:25 -0400

    Test use_targets()

--- tests/interactive/test-use_targets.R
+++ tests/interactive/test-use_targets.R
@@ -1,1 +1,6 @@
-use_targets()
+use_targets(open = FALSE) # Should not open the file.
+use_targets(open = TRUE) # Should open the file for editing.
+expect_true(file.exists("_targets.R"))
+unlink("_targets.Rmd")
+unlink("*.tmpl")
+unlink("run*")
