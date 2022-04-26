0701f9c69e219281dbb39832b5a1e005efe14a63
wlandau <will.landau@gmail.com>
2022-04-26 11:55:20 -0400

    Recursive source

--- inst/pipelines/use_targets.R
+++ inst/pipelines/use_targets.R
@@ -21,7 +21,7 @@ CLUSTERMQ
 FUTURE
 
 # Load the R scripts with your custom functions:
-for (file in list.files("R", full.names = TRUE)) source(file)
+for (file in list.files("R", full.names = TRUE, recursive = TRUE)) source(file)
 # source("other_functions.R") # Source other scripts as needed. # nolint
 
 # Replace the target list below with your own:
