cc952e6f47c6f17dbd7f01eaec9a2338750ee4d2
wlandau <will.landau@gmail.com>
2022-06-03 13:51:27 -0400

    Add run script

---
+++ inst/run/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Submit the pipeline as a background process with ./run.sh
+nohup nice -4 R CMD BATCH run.R &
+
+# Change the nice level above as appropriate
+# for your situation and system.
+
+# Removing .RData is recommended.
+# rm -f .RData
