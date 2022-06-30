45c104cb5bb1a6d484dec540994730c27d39ab28
wlandau <will.landau@gmail.com>
2022-06-30 12:54:49 -0400

    Fix #855

--- R/class_resources_aws.R
+++ R/class_resources_aws.R
@@ -48,8 +48,52 @@ resources_validate.tar_resources_aws <- function(resources) {
   tar_assert_positive(resources$part_size %|||% 1e8)
   tar_assert_scalar(resources$endpoint %|||% "endpoint")
   tar_assert_chr(resources$endpoint %|||% "endpoint")
-  tar_assert_list(resources$args)
-  tar_assert_named(resources$args)
+  resources_aws_validate_args(resources$args)
+}
+
+resources_aws_validate_args <- function(args) {
+  tar_assert_list(args)
+  tar_assert_named(
+    args,
+    "informal args of tar_resources_aws() must have nonempty unique names."
+  )
+  names <- names(args)
+  illegal <- c(
+    setdiff(names(formals(tar_resources_aws)), "..."),
+    "bucket", "Bucket", "key", "Key",
+    "prefix", "region", "part_size", "endpoint",
+    "version", "VersionId", "body", "Body",
+    "metadata", "Metadata", "UploadId", "MultipartUpload",
+    "PartNumber"
+  )
+  illegal <- unique(illegal)
+  msg <- paste(
+    "arguments to tar_resources_aws() must not include:",
+    paste(illegal, collapse = ", ")
+  )
+  tar_assert_not_in(names, illegal, msg = msg)
+  client <- paws::s3()
+  legal <- c(
+    names(formals(client$head_object)),
+    names(formals(client$get_object)),
+    names(formals(client$delete_object)),
+    names(formals(client$put_object)),
+    names(formals(client$create_multipart_upload)),
+    names(formals(client$abort_multipart_upload)),
+    names(formals(client$complete_multipart_upload)),
+    names(formals(client$upload_part))
+  )
+  legal <- unique(legal)
+  msg <- paste(
+    "informal arguments (via ...) in tar_resources_aws() must be:",
+    "arguments to paws::s3() functions: head_object(), get_object(),",
+    "delete_object(), put_object(), create_multipart_upload(),",
+    "abort_multipart_upload(), complete_multipart_upload(),",
+    "or upload_part(). Documentation of these functions,",
+    "including lists and explanations of the arguments,",
+    "are linked from https://paws-r.github.io/docs/s3/"
+  )
+  tar_assert_in(names, legal, msg)
 }
 
 #' @export
---
+++ tests/aws/test-aws_bad_args.R
@@ -0,0 +1,32 @@
+# Use sparingly to minimize AWS costs.
+# Verify all `targets` buckets are deleted afterwards.
+tar_test("aws_parquet format returns data frames", {
+  skip_if_no_aws()
+  skip_if_not_installed("arrow")
+  s3 <- paws::s3()
+  bucket_name <- random_bucket_name()
+  s3$create_bucket(Bucket = bucket_name)
+  on.exit(aws_s3_delete_bucket(bucket_name))
+  expr <- quote({
+    tar_option_set(
+      resources = tar_resources(
+        aws = tar_resources_aws(
+          bucket = !!bucket_name,
+          ExpectedBucketOwner = "phantom_f4acd87c52d4e62b"
+        )
+      ),
+      format = "parquet",
+      repository = "aws"
+    )
+    list(
+      tar_target(x, data.frame(x = seq_len(2), y = seq_len(2)))
+    )
+  })
+  expr <- tar_tidy_eval(expr, environment(), TRUE)
+  eval(as.call(list(`tar_script`, expr, ask = FALSE)))
+  expect_silent(tar_manifest(callr_function = NULL))
+  expect_error(
+    tar_make(callr_function = NULL),
+    class = "tar_condition_run"
+  )
+})
