498a344d37a128b63c370ef351d3a0766edb74ae
wlandau <will.landau@gmail.com>
2023-03-08 06:57:49 -0500

    Skip test on CRAN

--- tests/testthat/test-class_build.R
+++ tests/testthat/test-class_build.R
@@ -107,12 +107,14 @@ tar_test("validate good builds", {
 })
 
 tar_test("validate builds with bad metrics", {
+  skip_cran()
   build <- build_init(quote(1L + 1L), baseenv())
   build$metrics$seconds <- NULL
   expect_error(build_validate(build), class = "tar_condition_validate")
 })
 
 tar_test("encoding issue error handling", {
+  skip_cran()
   suppressWarnings(
     build <- build_init(
       quote(warning("<<\"pT\xbf\xbfD\x80QY\x94C\xd1")),
