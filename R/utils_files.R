5929d8eb7c6b9446918a2e46d3cfa6b516f1bbf6
wlandau <will.landau@gmail.com>
2023-04-29 17:09:24 -0400

    Sketch file_exists_runtime() and file_info_runtime()

--- R/utils_files.R
+++ R/utils_files.R
@@ -4,3 +4,17 @@ dir_create <- function(x) {
   }
   invisible()
 }
+
+file_exists_runtime <- function(x) {
+  out <- counter_exist_names(tar_runtime$file_exist, x)
+  out[!out] <- file.exists(x[!out])
+  out
+}
+
+file_info_runtime <- function(x) {
+  if_any(
+    all(counter_exist_names(tar_runtime$file_info_exist, x)),
+    tar_runtime$file_info[x,, drop = FALSE], # nolint
+    file.info(x, extra_cols = FALSE)
+  )
+}
