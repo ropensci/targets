3a3d5bd79ab0cfca3110702010ffc933cac7ee62
wlandau <will.landau@gmail.com>
2023-04-29 17:11:19 -0400

    Sketch tests of file_exists_runtime() and file_info_runtime()

---
+++ tests/testthat/test-utils_files.R
@@ -0,0 +1,23 @@
+tar_test("file_exists_runtime() with all files registered", {
+  
+})
+
+tar_test("file_exists_runtime() with not all files registered", {
+  
+})
+
+tar_test("file_exists_runtime() with no files registered", {
+  
+})
+
+tar_test("file_info_runtime() with all files registered", {
+  
+})
+
+tar_test("file_info_runtime() with not all files registered", {
+  
+})
+
+tar_test("file_info_runtime() with no files registered", {
+  
+})
