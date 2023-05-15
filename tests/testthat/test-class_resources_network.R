106d98de28beb6fc0cdfb98ad6885177da349543
wlandau <will.landau@gmail.com>
2023-05-15 14:33:41 -0400

    test network resources

--- tests/testthat/test-class_resources_network.R
+++ tests/testthat/test-class_resources_network.R
@@ -7,3 +7,9 @@ tar_test("tar_resources_network bad field", {
   x <- resources_network_init(max_tries = character(0))
   expect_error(resources_validate(x), class = "tar_condition_validate")
 })
+
+tar_test("print", {
+  x <- resources_network_init()
+  out <- utils::capture.output(print(x))
+  expect_true(any(grepl("tar_resources_network", out)))
+})
