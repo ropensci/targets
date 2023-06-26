22bb49c14389db7b7a11ba3b8d7395af4a674bf2
wlandau-lilly <will.landau@lilly.com>
2023-06-26 12:10:34 -0400

    test

--- tests/testthat/test-tar_prune_list.R
+++ tests/testthat/test-tar_prune_list.R
@@ -9,8 +9,10 @@ tar_test("tar_prune_list()", {
   }, ask = FALSE)
   tar_make(callr_function = NULL)
   tar_script(list(tar_target(y1, 1 + 1)), ask = FALSE)
-  out <- tar_prune_list(callr_function = NULL)
-  expect_equal(sort(out), sort(c("y2", "z")))
+  for (index in seq_len(2L)) {
+    out <- tar_prune_list(callr_function = NULL)
+    expect_equal(sort(out), sort(c("y2", "z")))
+  }
   tar_prune(callr_function = NULL)
   expect_equal(tar_prune_list(callr_function = NULL), character(0L))
 })
