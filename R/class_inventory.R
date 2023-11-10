cfee2e3fc135a3b409226239c949b04553c0fd2f
wlandau-lilly <will.landau@lilly.com>
2023-11-10 14:57:28 -0500

    cache more nuanced inventory

--- R/class_inventory.R
+++ R/class_inventory.R
@@ -1,3 +1,6 @@
+# This is an abstract inventory. Methods cache_key() and
+# cache_prefix() are for testing only. Only the subclasses
+# have serious versions of these methods.
 inventory_init <- function() {
   out <- inventory_new()
   out$reset()
@@ -14,25 +17,45 @@ inventory_class <- R6::R6Class(
   portable = FALSE,
   cloneable = FALSE,
   public = list(
-    hashes = NULL,
+    cache = NULL,
+    cache_key = function(key, bucket, store) {
+      name <- self$name(key = key, bucket = bucket)
+      self$cache[[name]] <- paste(store$file$hash, "key")
+    },
+    cache_prefix = function(key, bucket, store) {
+      name <- self$name(key = key, bucket = bucket)
+      self$cache[[name]] <- paste(store$file$hash, "prefix")
+    },
+    name = function(key, bucket) {
+      paste(bucket, key, sep = "|")
+    }
+    hash = function(key, bucket, version, store) {
+      name <- self$name(key = key, bucket = bucket)
+      if (!exists(x = name, envir = self$cache)) {
+        if (is.null(version)) {
+          self$cache_prefix(
+            key = key,
+            bucket = bucket,
+            store = store
+          )
+        } else {
+          self$cache_key(
+            key = key,
+            bucket = bucket,
+            store = store
+          )
+        }
+      } 
+      self$cache[[name]]
+    },
     reset = function() {
-      self$hashes <- new.env(parent = emptyenv())
+      self$cache <- new.env(parent = emptyenv())
     },
     list = function() {
-      names(self$hashes)
-    },
-    hash = function(store) {
-      store_validate(store)
-      self$update(store)
-      self$hashes[[store$file$path]]
-    },
-    update = function(store) {
-      store_validate(store)
-      self$hashes[[store$file$path]] <- store$file$hash
-      invisible()
+      names(self$cache)
     },
     validate = function() {
-      tar_assert_envir(self$hashes)
+      tar_assert_envir(self$cache)
       invisible()
     }
   )
