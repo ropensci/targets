5956f3d33c649298521aaeb689c85ac109a88f60
wlandau <will.landau@lilly.com>
2024-02-06 16:26:05 -0500

    ns

--- tests/hpc/test-crew_sge.R
+++ tests/hpc/test-crew_sge.R
@@ -2,14 +2,13 @@ tar_test("crew SGE with many tasks and many workers", {
   skip_on_cran()
   skip_if_not_installed("crew.cluster")
   tar_script({
-    library(crew.cluster)
     library(targets)
-    controller <- crew_controller_sge(
+    controller <- crew.cluster::crew_controller_sge(
       workers = 25,
       tasks_max = 100,
       script_lines = "module load R/4.2.2"
     )
-    tar_option_set(controller = crew_controller_group(controller))
+    tar_option_set(controller = crew::crew_controller_group(controller))
     list(
       tar_target(x, seq_len(10000)),
       tar_target(y, Sys.sleep(0.1), pattern = map(x))
