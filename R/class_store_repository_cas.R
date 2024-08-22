a5ca1332674f3482478e2a5e63b423be35160ddd
wlandau <will.landau.oss@gmail.com>
2024-08-22 14:45:17 -0400

    inherit methods

--- R/class_store_repository_cas.R
+++ R/class_store_repository_cas.R
@@ -4,6 +4,8 @@ store_class_repository.repository_cas <- function(repository, store, format) {
   c(
     sprintf("tar_repository_cas_%s", format),
     "tar_repository_cas",
+    "tar_cloud",
+    if_any("tar_external" %in% class(store), character(0), "tar_external"),
     class(store)
   )
 }
@@ -11,4 +13,3 @@ store_class_repository.repository_cas <- function(repository, store, format) {
 #' @export
 store_assert_repository_setting.repository_cas <- function(repository) {
 }
-
