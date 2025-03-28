6c32528a47c61a2a4d23522e77e56320e0dac7c9
wlandau <will.landau.oss@gmail.com>
2025-03-28 16:21:25 -0400

    shave off overhead from load_packages()

--- R/utils_packages.R
+++ R/utils_packages.R
@@ -1,23 +1,23 @@
 # require() is faster than library() # nolint
 # but we should still fail early and loudly when needed.
 load_packages <- function(packages, library) {
-  out <- suppressPackageStartupMessages(
-    lapply(
-      packages,
-      require,
-      lib.loc = library,
-      quietly = TRUE,
-      character.only = TRUE
-    )
+  out <- lapply(
+    packages,
+    require,
+    lib.loc = library,
+    quietly = TRUE,
+    character.only = TRUE
   )
   out <- as.logical(unlist(out))
-  msg <- paste(
-    "could not find packages",
-    paste(packages[!out], collapse = ", "),
-    "in library paths",
-    paste(library, collapse = ", ")
-  )
-  tar_assert_true(all(out), msg)
+  if (!all(out)) {
+    msg <- paste(
+      "could not find packages",
+      paste(packages[!out], collapse = ", "),
+      "in library paths",
+      paste(library, collapse = ", ")
+    )
+    tar_throw_validate(msg)
+  }
 }
 
 package_installed <- function(package) {
