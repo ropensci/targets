756073ea9df9fa53273041e2b664230c4c5d5557
wlandau <will.landau.oss@gmail.com>
2025-04-01 15:37:10 -0400

    Increase responsiveness of the progress bar

--- R/utils_callr.R
+++ R/utils_callr.R
@@ -283,6 +283,9 @@ tar_callr_args_default <- function(callr_function, reporter = NULL) {
     return(list())
   }
   out <- list(spinner = FALSE, stderr = "2>&1")
+  if (interactive() && !isTRUE(getOption("knitr.in.progress"))) {
+    out$env <- c(callr::rcmd_safe_env(), CLI_TICK_TIME = "200") # nocov
+  }
   out[intersect(names(out), names(formals(callr_function)))]
 }
 
