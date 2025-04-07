9d9192c18e51da4d600960d52145bcb76c5af608
wlandau <will.landau.oss@gmail.com>
2025-04-07 08:48:58 -0400

    sketch faster sequential queue

--- R/class_sequential.R
+++ R/class_sequential.R
@@ -1,9 +1,9 @@
-sequential_init <- function(names = character(0)) {
-  sequential_new(names)
+sequential_init <- function(names = character(0), step = 1e5L) {
+  sequential_new(data = names, step = step)
 }
 
-sequential_new <- function(data = NULL) {
-  sequential_class$new(data)
+sequential_new <- function(data = NULL, step = NULL) {
+  sequential_class$new(data = data, step = step)
 }
 
 sequential_class <- R6::R6Class(
@@ -14,37 +14,78 @@ sequential_class <- R6::R6Class(
   cloneable = FALSE,
   public = list(
     head = NULL,
-    initialize = function(data) {
+    tail = NULL,
+    step = NULL,
+    initialize = function(data = NULL, step = NULL) {
       super$initialize(data = data)
-      self$head <- 1L
+      self$tail <- length(data)
+      self$head <- as.integer(self$tail > 0L)
+      self$step <- as.integer(step)
     },
     is_nonempty = function() {
-      .subset2(self, "head") <= length(.subset2(self, "data"))
-    },
-    dequeue = function() {
-      index <- .subset2(self, "head")
-      data <- .subset2(self, "data")
-      if (index <= length(data)) {
-        self$head <- index + 1L
-      }
-      .subset(data, index)
-    },
-    prepend = function(names, ranks = NULL) {
-      data <- .subset2(self, "data")
-      out <- c(data[seq_len(head - 1L)], names)
-      if (head <= length(data)) {
-        out <- c(out, data[seq(from = head, to = length(data), by = 1L)])
-      }
-      self$data <- out
-    },
-    append = function(names, ranks = NULL) {
-      self$data <- c(self$data, names)
+      head <- .subset2(self, "head")
+      tail <- .subset2(self, "tail")
+      tail > 0L && head <= tail
     },
     should_dequeue = function() {
-      self$is_nonempty()
+      head <- .subset2(self, "head")
+      tail <- .subset2(self, "tail")
+      tail > 0L && head <= tail
+    },
+    clean = function() {
+      if (.subset2(self, "is_nonempty")()) {
+        head <- .subset2(self, "head")
+        tail <- .subset2(self, "tail")
+        self$data <- .subset2(self, "data")[seq(from = head, to = tail)]
+        self$tail <- tail - head
+        self$head <- 1L
+      } else {
+        self$data <- character(0L)
+        self$head <- 0L
+        self$tail <- 0L
+      }
+    },
+    extend = function() {
+      .subset2(self, "clean")()
+      self$data <- c(.subset2(self, "data"), rep(NA_character_, self$step))
+    },
+    dequeue = function() {
+      if (.subset2(self, "is_nonempty")()) {
+        head <- .subset2(self, "head")
+        out <- .subset(.subset2(self, "data"), head)
+        self$head <- head + 1L
+        return(out)
+      } else {
+        return(NULL)
+      }
+    },
+    append = function(names, ranks = NULL) {
+      data <- .subset2(self, "data")
+      tail <- .subset2(self, "tail")
+      if (length(data) - tail < length(names)) {
+        .subset2(self, "extend")()
+      }
+      index <- 1L
+      n <- length(names)
+      data <- .subset2(self, "data")
+      tail <- .subset2(self, "tail")
+      while (index <= n) {
+        self$data[index + tail] <- .subset(names, index)
+        index <- index + 1L
+      }
+      self$tail <- tail + n
+    },
+    prepend = function(names, ranks = NULL) {
+      .subset2(self, "clean")()
+      self$data <- c(.subset2(self, "data"), names)
+      self$tail <- .subset2(self, "tail") + length(names)
     },
     validate = function() {
       tar_assert_chr(self$data)
+      tar_assert_int(self$head)
+      tar_assert_int(self$tail)
+      tar_assert_int(self$step)
+      tar_assert_true(self$head <= self$tail)
     }
   )
 )
