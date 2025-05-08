5801a1e62a0a47d9e935328df47d3a9a98a10fb8
wlandau <will.landau.oss@gmail.com>
2025-05-08 08:21:44 -0400

    Eliminate bottleneck in meta()

--- R/class_meta.R
+++ R/class_meta.R
@@ -66,29 +66,28 @@ meta_class <- R6::R6Class(
       self$database$list_rows()
     },
     restrict_records = function(pipeline) {
-      names_envir <- names(pipeline$imports)
-      names_records <- self$list_records()
-      index <- 1L
-      n <- length(names_records)
       bar <- cli_local_progress_bar_init(
-        label = "selecting metadata",
-        total = n
+        label = "restricting metadata records"
       )
       on.exit(cli_local_progress_bar_destroy(bar = bar))
-      get_row <- self$database$get_row
-      is_branch <- vector(mode = "logical", length = n)
-      while (index <= n) {
-        name <- .subset(names_records, index)
-        is_branch[index] <- .subset2(get_row(name), "type") == "branch"
-        cli_local_progress_bar_update(bar = bar, index = index)
-        index <- index + 1L
-      }
-      names_children <- names_records[is_branch]
-      names_targets <- pipeline_get_names(pipeline)
-      names_parents <- intersect(names_records, names_targets)
-      names_current <- c(names_envir, names_targets, names_children)
-      remove <- setdiff(names_records, names_current)
+      names_envir <- names(pipeline$imports)
+      names_pipeline <- pipeline_get_names(pipeline)
+      types <- unlist(
+        lapply(names_pipeline, function(name) get_row(name)$type)
+      )
+      names_stems <- names_pipeline[types == "stem"]
+      names_patterns <- names_pipeline[types == "pattern"]
+      names_branches <- unlist(
+        lapply(
+          names_patterns,
+          function (name) get_row(name)$children
+        )
+      )
+      names_builders <- c(names_stems, names_branches)
+      names_current <- c(names_envir, names_patterns, names_builders)
+      remove <- setdiff(self$list_records(), names_current)
       self$del_records(remove)
+      self$local_builders <- intersect(self$local_builders, names_builders)
     },
     hash_deps = function(deps, pipeline) {
       hash_list <- .subset2(self, "produce_hash_list")(deps, pipeline)
