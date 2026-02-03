613ce0aab2fc40105055c201da773af0a95ed017
wlandau <will.landau.oss@gmail.com>
2026-02-03 15:47:49 -0500

    poll cleanup() to avoid warnings about unclean shutdowns from clustermq

--- R/class_clustermq.R
+++ R/class_clustermq.R
@@ -238,7 +238,9 @@ clustermq_class <- R6::R6Class(
     run_clustermq = function() {
       on.exit({
         if (!is.null(self$worker_list)) {
-          try(self$worker_list$cleanup())
+          while (!suppressMessages(self$worker_list$cleanup())) {
+            Sys.sleep(0.1)
+          }
         }
       })
       self$start_workers()
